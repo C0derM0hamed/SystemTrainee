1a884baa664635424b6f48b9ae722f6249d85a4f
Mohamed Mostafa <mohamed@example.com>
2026-04-19 23:14:19 +0200

    edit: edit on url to make / redirect to list page

--- SystemTrainee/urls.py
+++ SystemTrainee/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('trainee/', include('trainee.urls')),
+    path('', include('trainee.urls')), 
 ]
